2415e83aac7a5de39896d10a0baabd5b7a72f158
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-01-30 11:39:28 +0800

    Host

--- QPHost.cpp
+++ QPHost.cpp
@@ -1,4 +1,18 @@
 import qps-r
+import code runner
 
 if qphost == {host.false = (active)};
   then change {host.true = (not.active)};
+
+// Host Process
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "The host process has started in QPS-R.";
+    
+// Do not fork and delete this code or your forked QPS-R respitory will be moderated by the QPS-R team.
+    if system.true = 1;
+    if system.false = 0;
+    
+    while system == is.true:
+  code runner = .run
